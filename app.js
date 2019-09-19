c2ad0a6dabe03420a64fc4376c558bd231ee6fcb
FareedKandalaft <fareed@spectrecoder.com>
2019-09-18 21:57:01 -0400

    AppController and UICtrler basic. DOMStrings var

--- app.js
+++ app.js
@@ -8,25 +8,50 @@ var BudgetController = (function() {
 
 // UI CONTROLLER
 var UIController = (function() {
+  var DOMStrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    addBtn: ".add__btn"
+  };
+  // to make the closure's data available you
+  // return an "object/function"
+  return {
+    getInput: function() {
+      // Here the functions return is an object with properties
+      // note the use of : as a name : val paradigm
+      return {
+        type : document.querySelector(DOMStrings.inputType).value, // either inc or exp
+        description : document.querySelector(DOMStrings.inputDescription).value,
+        value : document.querySelector(DOMStrings.inputValue).value
+      };
+    }, // why no ; here?
 
-
-})();
+    getDOMstrings: function () {
+      return DOMStrings;
+    }
+  };
+}    )(); // hmm... apparently functions/iFFE dont use ;
 
 
 
 // GLOBAL APP CONTROLLER
-var AppController = (function() {
+var AppController = (function(budgetCtrl, UICtrl) {
+
+  var DOM = UICtrl.getDOMstrings();
 
   var ctrlAddItem = function() {
     // 1. Get input data
+    var input = UICtrl.getInput();
+    console.log(input);
     // 2. Add item to the budget CONTROLLER
     // 3. add item to UI
     // 4. calc the budget
     // 5. display the budget on the UI
-    console.log(event);
+
   };
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  document.querySelector(DOM.addBtn).addEventListener('click', ctrlAddItem);
 
   // sample capturing the keypress event
   // because its not related to a specific CONTROL
